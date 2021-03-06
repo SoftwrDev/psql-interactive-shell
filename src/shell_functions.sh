d1660574fb3fd975fb8ad34281291c1e26dcc4c7
Gabriel da Silva Santos <32193242+SoftwrDev@users.noreply.github.com>
2021-03-06 19:47:47 -0300

    chore: simplify code structure

--- src/shell_functions.sh
+++ src/shell_functions.sh
@@ -1,60 +1,56 @@
+  GNU nano 4.8                                            src/shell_functions.sh                                                      
 #!/bin/bash
 
 checkEmptyCredentials() {
-	if [[ ${#LOGIN} -eq 0 && ${#PGUSER} -eq 0 || ${#PASSWORD} -eq 0 && ${#PGPASSWORD} -eq 0 ]]; then
-    		echo "Invalid credentials"
-    		exit 1
-	fi
+        if [[ ${#LOGIN} -eq 0 && ${#PGUSER} -eq 0 || ${#PASSWORD} -eq 0 && ${#PGPASSWORD} -eq 0 ]]; then
+                echo "Invalid credentials"
+                exit 1
+        fi
 }
 
 checkEmptyDatabaseProvided() {
-	if [[ ${#DBNAME} -eq 0 && ${#PGDATABASE} -eq 0 ]]; then
-		echo "Database name not provided"
-		exit 1
-	fi
+        if [[ ${#DBNAME} -eq 0 && ${#PGDATABASE} -eq 0 ]]; then
+                echo "Database name not provided"
+                exit 1
+        fi
 }
 
 prepareEnv() {
-	if [[ ${#PGUSER} -eq 0 ]]; then
-		export PGUSER=$LOGIN
-	fi
+        if [[ ${#PGUSER} -eq 0 ]]; then
+                export PGUSER=$LOGIN
+        fi
 
-	if [[ ${#PGDATABASE} -eq 0 ]]; then
-		export PGDATABASE=$DBNAME
-	fi
+        if [[ ${#PGDATABASE} -eq 0 ]]; then
+                export PGDATABASE=$DBNAME
+        fi
 
-	if [[ ${#PGPASSWORD} -eq 0 ]]; then
-		export PGPASSWORD=$PASSWORD
-	fi
+        if [[ ${#PGPASSWORD} -eq 0 ]]; then
+                export PGPASSWORD=$PASSWORD
+        fi
 
-	if [[ ${#DB_HOST} -eq 0 ]]; then
-		if [[ ${#PGHOST} -eq 0 ]]; then 
-			export PGHOST=127.0.0.1
-		fi
-	else
-		export PGHOST=$DB_HOST
-	fi
+        if [[ ${#DB_HOST} -eq 0 && ${#PGHOST} -eq 0 ]]; then
+                export PGHOST=127.0.0.1
+        else
+                export PGHOST=$DB_HOST
+        fi
 
-	if [[ ${#DB_PORT} -eq 0 ]]; then
-		if [[ $PGPORT -eq 0 ]]; then
-			export PGPORT=5432
-		fi
-	else
-		export PGPORT=$DB_PORT
-	fi
+        if [[ ${#DB_PORT} -eq 0 && $PGPORT -eq 0 ]]; then
+                export PGPORT=5432
+        else
+                export PGPORT=$DB_PORT
+        fi
 
-	if [[ ${#CODE_EDITOR} -eq 0 ]]; then
-			export CODE_EDITOR="nano"
-	fi
+        if [[ ${#CODE_EDITOR} -eq 0 ]]; then
+                export CODE_EDITOR="nano"
+        fi
 }
 
 
 testPsqlConnectivity() {
-	psql -c "\l" > /dev/null 2>&1
+        psql -c "\l" > /dev/null 2>&1
 
-	if [[ $? -ne 0 ]]; then
-		printf "Unable to connect to database. Check if the database is really available\n\n"
-		exit 1
-	fi
+        if [[ $? -ne 0 ]]; then
+                printf "Unable to connect to database. Check if the database is really available\n\n"
+                exit 1
+        fi
 }
-
