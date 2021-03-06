c4f7cfa4cbfad8e37174f170b9a6bd90b18817f7
SoftwrDev <contato.santosgabriel@gmail.com>
2021-03-06 18:28:35 -0300

    chore: refactor env variables setting

--- shell_functions.sh
+++ shell_functions.sh
@@ -14,11 +14,29 @@ checkEmptyDatabaseProvided() {
 	fi
 }
 
-checkCodeEditor() {
-	if [[ ${#CODE_EDITOR} -eq 0 ]]; then
-		export CODE_EDITOR="nano"
+prepareEnv() {
+	export PGUSER=$LOGIN
+	export PGDATABASE=$DBNAME
+	export PGPASSWORD=$PASSWORD
+
+	if [[ ${#DB_HOST} -eq 0 ]]; then
+		export PGHOST=127.0.0.1
+	else
+		export PGHOST=$DB_HOST
 	fi
+
+	if [[ ${#DB_PORT} -eq 0 ]]; then
+		export PGPORT=5432
+	else
+		export PGPORT=$DB_PORT
+	fi
+
+        if [[ ${#CODE_EDITOR} -eq 0 ]]; then
+                export CODE_EDITOR="nano"
+        fi
+
 }
+
 
 testPsqlConnectivity() {
 	psql -c "\l" > /dev/null 2>&1
